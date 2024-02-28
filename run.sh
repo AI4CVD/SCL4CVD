bb2c777c7fd2c8033ad0aaa40531030a21f1e4a0
AI4CVD <49785967+AI4CVD@users.noreply.github.com>
2024-02-28 11:30:12 +0800

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,44 @@
+python run.py \
+    --output_dir=./saved_models \
+    --tokenizer_name=./codebert \
+    --model_name_or_path=./codebert \
+    --do_train \
+    --train_data_file=../dataset/train.jsonl \
+    --eval_data_file=../dataset/valid.jsonl \
+    --test_data_file=../dataset/test.jsonl \
+    --epoch 5 \
+    --alpha 3 \
+    --dropout_probability 0.03 \
+    --lam 0.3 \
+    --code_length 256 \
+    --data_flow_length 64 \
+    --block_size 400 \
+    --train_batch_size 32 \
+    --eval_batch_size 64 \
+    --learning_rate 2e-5 \
+    --max_grad_norm 1.0 \
+    --evaluate_during_training \
+    --seed 123456  2>&1 | tee train_devign.log
+
+python run.py \
+    --output_dir=./saved_models \
+    --tokenizer_name=./codebert \
+    --model_name_or_path=./codebert \
+    --do_eval \
+    --do_test \
+    --train_data_file=../dataset/train.jsonl \
+    --eval_data_file=../dataset/valid.jsonl \
+    --test_data_file=../dataset/test.jsonl \
+    --epoch 5 \
+    --alpha 3 \
+    --dropout_probability 0.03 \
+    --lam 0.3 \
+    --code_length 256 \
+    --data_flow_length 64 \
+    --block_size 400 \
+    --train_batch_size 32 \
+    --eval_batch_size 64 \
+    --learning_rate 2e-5 \
+    --max_grad_norm 1.0 \
+    --evaluate_during_training \
+    --seed 123456  2>&1 | tee test_devign.log
